a48c461e3084f22d7bcd4990080fa83d870515b1
anas melk <anasmelk@192.168.1.4>
2022-04-17 12:41:38 +0000

    ported app to typescript

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -3,23 +3,29 @@ import Head from "next/head";
 import Date from "../../components/date";
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import { GetStaticPaths, GetStaticProps } from 'next';
 
-export async function getStaticPaths() {
+export const  getStaticPaths:GetStaticPaths = async() =>{
   const paths = getAllPostIds();
   return {
     paths,
     fallback: false,
   };
 }
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id);
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id as string)
   return {
     props: {
-      postData,
-    },
-  };
+      postData
+    }
+  }
 }
-export default function Post({ postData }) {
+export default function Post({ postData }: {
+  postData: {
+    title: string
+    date: string
+    contentHtml: string
+  }}) {
     return (
       <Layout>
         <Head>
